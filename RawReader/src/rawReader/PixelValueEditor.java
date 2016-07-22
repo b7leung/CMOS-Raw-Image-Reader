4c409220935cc6ce61ff821e742bfeabf94cc83b
Brandon Leung <brandonl9981@yahoo.com>
2016-07-21 22:15:49 -0700

    Added error checking

--- RawReader/src/rawReader/PixelValueEditor.java
+++ RawReader/src/rawReader/PixelValueEditor.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
@@ -21,83 +22,92 @@ public class PixelValueEditor implements ActionListener{
     private int xPixel;
     private int yPixel;
     private ProcessRaw image;
-    private JLabel PixelValue;
+    private JLabel PixelValueLabel;
+    private JFrame PixelValueEditorFrame;
     
     public PixelValueEditor(ProcessRaw image){
         this.image = image;
     }
 
+    /**
+     * @wbp.parser.entryPoint
+     */
     public void make(){
-        JFrame frmPixelValueEditor = new JFrame("demo");
-        frmPixelValueEditor.setTitle("Pixel Value Editor");
-        frmPixelValueEditor.setSize( 548,370);
-        frmPixelValueEditor.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        PixelValueEditorFrame = new JFrame("demo");
+        PixelValueEditorFrame.setTitle("Pixel Value Editor");
+        PixelValueEditorFrame.setSize( 548,370);
+        PixelValueEditorFrame.setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
         
-        JPanel panel = new JPanel();
-        frmPixelValueEditor.getContentPane().add(panel, BorderLayout.NORTH);
+        JPanel inputPanel = new JPanel();
+        PixelValueEditorFrame.getContentPane().add(inputPanel, BorderLayout.NORTH);
         
-        JLabel lblX = new JLabel("x");
-        panel.add(lblX);
+        JLabel xLabel = new JLabel("x:");
+        inputPanel.add(xLabel);
         
         xTextField = new JTextField();
-        panel.add(xTextField);
+        inputPanel.add(xTextField);
         xTextField.setColumns(10);
         
-        JLabel lblY = new JLabel("y");
-        panel.add(lblY);
+        JLabel yLabel = new JLabel("y:");
+        inputPanel.add(yLabel);
         
         yTextField = new JTextField();
-        panel.add(yTextField);
+        inputPanel.add(yTextField);
         yTextField.setColumns(10);
         
-        findButton = new JButton("find");
-        panel.add(findButton);
+        findButton = new JButton("Find");
+        inputPanel.add(findButton);
         findButton.addActionListener( this );
         
         
-        JPanel panel_1 = new JPanel();
-        frmPixelValueEditor.getContentPane().add(panel_1, BorderLayout.CENTER);
+        JPanel infoPanel = new JPanel();
+        PixelValueEditorFrame.getContentPane().add(infoPanel, BorderLayout.CENTER);
         
-        PixelValue = new JLabel("Pixel Value: ");
-        panel_1.add(PixelValue);
-        
-        JLabel currentPixelValue = new JLabel("0");
-        panel_1.add(currentPixelValue);
+        PixelValueLabel = new JLabel("Pixel Value: ");
+        infoPanel.add(PixelValueLabel);
         
         changeButton = new JButton("Change");
-        panel_1.add(changeButton);
-        
-        JPanel panel_2 = new JPanel();
-        frmPixelValueEditor.getContentPane().add(panel_2, BorderLayout.SOUTH);
+        infoPanel.add(changeButton);
 
-        frmPixelValueEditor.setVisible( true );
+        PixelValueEditorFrame.setVisible( true );
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==findButton){
+            if(xTextField.getText().equals( "" )){
+                
+            }
             xPixel = Integer.parseInt( xTextField.getText() );
             yPixel = Integer.parseInt(  yTextField.getText() );
             boolean valid = true;
             if(xPixel >= image.getWidth() ){
-                System.out.println( "Error: " + xPixel + " is too big for the image width of " + image.getWidth() );
+                JOptionPane.showMessageDialog( PixelValueEditorFrame,
+                "Error: " + xPixel + " is too big for the image width of " + image.getWidth(),
+                "Error", JOptionPane.PLAIN_MESSAGE);
                 valid = false;
             }
             if (xPixel < 0){
-                System.out.println( "Error: pixel's x-coordinate cannot be negative" ); 
+                JOptionPane.showMessageDialog( PixelValueEditorFrame,
+                "Error: pixel's x-coordinate cannot be negative",
+                "Error", JOptionPane.PLAIN_MESSAGE);
                 valid = false;
             }
             if ( yPixel >= image.getWidth() ){
-                System.out.println( "Error: " + yPixel + " is too big for the image height of " + image.getHeight() );
+                JOptionPane.showMessageDialog( PixelValueEditorFrame,
+                "Error: " + yPixel + " is too big for the image height of " + image.getHeight(),
+                "Error", JOptionPane.PLAIN_MESSAGE);
                 valid = false;
             }
             if ( yPixel < 0 ){
-                System.out.println( "Error: pixel's y-coordinate cannot be negative" ); 
+                JOptionPane.showMessageDialog( PixelValueEditorFrame,
+                "Error: pixel's y-coordinate cannot be negative",
+                "Error", JOptionPane.PLAIN_MESSAGE);
                 valid = false;
-            } 
+            }
             if(valid){
                 try {
-                    PixelValue.setText( ""+image.getPixelValue( xPixel, yPixel ) );
+                    PixelValueLabel.setText( "Pixel Value: "+image.getPixelValue( xPixel, yPixel ) );
                 } catch (IOException e1) {
                     System.err.println( "Pixel Data could not be read." );
                 }
